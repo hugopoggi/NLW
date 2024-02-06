6ae3a248f6ac9dc8a94c78cd8e891f09e04f99b3
Hugo Poggi <hugopoggiluciano@gmail.com>
2024-02-06 20:54:05 -0300

    Final 2 aula

--- src/views/http_types/http_request.py
+++ src/views/http_types/http_request.py
@@ -1,12 +1,13 @@
 from typing import Dict
 
 class HttpRequest:
-    def ___init__(
-              self,
-              header: Dict = None, 
-              body: Dict = None, 
-              query_params: Dict = None
-         ) -> None:
+    def __init__(
+            self,
+            header: Dict = None,
+            body: Dict = None,
+            query_params: Dict = None
+        ) -> None:
         self.header = header
         self.body = body
         self.query_params = query_params
+    
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,18 +1,21 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_reponse import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
-        reponsability for interecting with HTTP
+       responsability for interacting with HTTP 
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        #body = http_request.body
-        #product_code = body ["product_code"]
+        tag_creator_controller = TagCreatorController()
 
+        body = http_request.body
+        product_code = body["product_code"]
 
-        print('Estou na minha view')
-        print(http_request)
+        # logica de regra de negocio
+        formatted_response = tag_creator_controller.create(product_code)
 
-        return HttpResponse(status_code=200, body={"hello": "World"})
+        # retorno http
+        return HttpResponse(status_code=200, body=formatted_response)
     
